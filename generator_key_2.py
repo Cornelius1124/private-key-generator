30a38e586a78eb7bd1bf409f260e03e63762b563
Chester ETK <149551747+Cornelius1124@users.noreply.github.com>
2025-06-27 11:36:07 +0700

    Add files via upload

---
+++ generator_key_2.py
@@ -0,0 +1,17 @@
+from ecdsa import SECP256k1, SigningKey
+
+sk = SigningKey.generate(curve=SECP256k1)
+vk = sk.verifying_key
+
+private_key_bytes = sk.to_string()
+private_key_int = int.from_bytes(private_key_bytes, byteorder='big')
+
+public_key_bytes = vk.to_string()
+public_key_hex = public_key_bytes.hex()
+
+print("Private Key (hex):", private_key_bytes.hex())
+print("Private Key (int):", private_key_int)
+print("Public Key (uncompressed):", public_key_hex)
+
+# Keeps the window open
+input("\nPress Enter to exit...")
